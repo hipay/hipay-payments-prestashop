b355d58b68000c2ab50d5edcf5bdc972b92af412
El Mekki ALOUI <ealoui.ext@hipay.com>
2026-07-13 14:20:19 +0000

    [PI-6101] Rendre propre le projet pour Renovate

--- hipaypayments.php
+++ hipaypayments.php
@@ -35,7 +35,7 @@ class HiPayPayments extends PaymentModule
     {
         $this->name = 'hipaypayments';
         $this->author = 'HiPay';
-        $this->version = '4.2.3';
+        $this->version = '4.2.4';
         $this->tab = 'payments_gateways';
         $this->ps_versions_compliancy = [
             'min' => '8.0',
